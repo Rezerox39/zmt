8e0cc5629f0776a93b502c078e11a420d936f765
Rezerox39 <rezerox39@users.noreply.github.com>
2026-07-28 13:21:25 +0000

    fix: add missing UserAgents import in YoutubeStreamResolver

--- app/src/main/java/dev/jyotiraditya/dmt/data/remote/youtube/YoutubeStreamResolver.kt
+++ app/src/main/java/dev/jyotiraditya/dmt/data/remote/youtube/YoutubeStreamResolver.kt
@@ -3,6 +3,7 @@ package dev.jyotiraditya.dmt.data.remote.youtube
 import android.util.Log
 import dev.jyotiraditya.dmt.data.remote.youtube.innertube.Innertube
 import dev.jyotiraditya.dmt.data.remote.youtube.innertube.models.Context
+import dev.jyotiraditya.dmt.data.remote.youtube.innertube.models.UserAgents
 import dev.jyotiraditya.dmt.data.remote.youtube.innertube.models.bodies.PlayerBody
 import dev.jyotiraditya.dmt.data.remote.youtube.innertube.requests.player
 import javax.inject.Inject
